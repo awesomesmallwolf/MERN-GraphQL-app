eea39c01f332a34d6f7a334f32528271383c95e7
fredcarterwolf <fredcarterwolf@gmail.com>
2021-12-31 01:07:34 +0100

    Configure Apollo Server
    Adjust server configuration by creating and configuring Apollo Server with predefined typeDefs and resolvers.
    From now on, server will listen at http://localhost:8080/graphql. It will open up Apollo Studio.

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,31 @@
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
 import http from 'http';
 require('dotenv').config();
 
 import './mongodb/connect';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 
 const startServer = async () => {
-  const { SERVER_HOST, SERVER_PORT } = process.env;
+  const { SERVER_HOST, SERVER_PORT, GRAPHQL_SERVER_PATH } = process.env;
   const app = express();
   const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
+  await server.start();
+  server.applyMiddleware({ app, path: `${GRAPHQL_SERVER_PATH}` });
   await new Promise((resolve) =>
     httpServer.listen({ port: SERVER_PORT }, resolve),
   );
-  console.log(`🚀 Server ready at ${SERVER_HOST}:${SERVER_PORT}`);
+  console.log(
+    `🚀 Server ready at ${SERVER_HOST}:${SERVER_PORT}${server.graphqlPath}`,
+  );
 };
 
 startServer();
