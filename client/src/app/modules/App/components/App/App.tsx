6f1d81642fb61c4abb0b6918e07d44edbc7095cf
fredcarterwolf <fredcarterwolf@gmail.com>
2021-12-31 11:54:30 +0100

    Setup client
    Client is set up with Create React App and runs at http://localhost:3000

---
+++ client/src/app/modules/App/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './App.scss';
+
+const App: React.FC = () => <div className="app-main-container">App</div>;
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,13 @@
+import { render } from 'react-dom';
+
+import { App } from 'app/modules/App';
+import reportWebVitals from './reportWebVitals';
+
+const renderApp = () => render(<App />, document.getElementById('app-root'));
+
+renderApp();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
