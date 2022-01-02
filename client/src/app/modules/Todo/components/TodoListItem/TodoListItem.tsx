7b483f184b51a82e26a522fb5c1a68bdf80dd382
fredcarterwolf <fredcarterwolf@gmail.com>
2022-01-02 23:51:00 +0100

    Add delete todo feature
    Create delete button with an icon from react-icons library for deleting the selected todo from the store by dispatching an action with the item's id.

--- client/src/app/modules/Todo/components/TodoListItem/TodoListItem.tsx
+++ client/src/app/modules/Todo/components/TodoListItem/TodoListItem.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Box,
+  Flex,
+  Icon,
+  IconButton,
+  ListItem,
+  Spacer,
+} from '@chakra-ui/react';
+import { RiDeleteBin7Line } from 'react-icons/ri';
 
 import { TodoItem } from 'app/store/models/todo';
-import { ListItem } from '@chakra-ui/react';
+import { Dispatch } from 'app/store';
 
-const TodoListItem: React.FC<TodoItem> = (todoItem) => (
-  <ListItem>
-    <Flex align="center">
-      <Box>{todoItem.label}</Box>
-    </Flex>
-  </ListItem>
-);
+const TodoListItem: React.FC<TodoItem> = (todoItem) => {
+  const dispatch = useDispatch<Dispatch>();
+
+  const handleDeleteTodo = useCallback(
+    () => dispatch.todo.delete(todoItem.id),
+    [dispatch, todoItem],
+  );
+
+  return (
+    <ListItem>
+      <Flex align="center">
+        <Box mr="2rem">{todoItem.label}</Box>
+
+        <Spacer />
+
+        <Box>
+          <IconButton
+            colorScheme="red"
+            variant="outline"
+            aria-label="Delete todo"
+            size="xs"
+            icon={<Icon as={RiDeleteBin7Line} />}
+            onClick={handleDeleteTodo}
+          />
+        </Box>
+      </Flex>
+    </ListItem>
+  );
+};
 
 export default TodoListItem;
